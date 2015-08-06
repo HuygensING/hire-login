204e07233212e291f56549ea7a482afc3305b900
Rene van der Ark <r.van.der.ark@gmail.com>
2015-08-06 13:50:42 +0200

    complete unit test for api.js

--- test/api.js
+++ test/api.js
@@ -1,5 +1,6 @@
 import sinon from "sinon";
 import should from "should";
+import serverActions from "../src/actions"
 import api from "../src/api";
 
 
@@ -8,23 +9,28 @@ global.tools = {};
 
 describe("api", function() {
 
-	it("Should call xhr with the proper headers for basic login", function() {
+	it("Should call xhr with the proper headers for basic login and call proper serverAction", function() {
 		sinon.stub(api, "performXhr", function(opts, callback) {
 			opts.method.should.equal("POST");
 			opts.uri.should.equal("dummy-url");
 			opts.headers.Authorization.should.equal("Basic btoa-mock-encoded dummy-user:dummy-pass");
+			callback.should.equal(serverActions.receiveBasicLogin);
 		});
 
 		api.basicLogin("dummy-url", "dummy-user", "dummy-pass");
+		api.performXhr.restore();
+	});
 
-/*	basicLogin(url, username, password) {
-		xhr({
-			method: 'POST',
-			uri: url,
-			headers: {
-				Authorization: 'Basic ' + btoa(username + ':' + password)
-			}
-		},	serverActions.receiveBasicLogin);
-	},*/
+	it("Should call xhr with the proper headers for fetching user data and call proper serverAction", function() {
+		sinon.stub(api, "performXhr", function(opts, callback) {
+			opts.method.should.equal("GET");
+			opts.uri.should.equal("dummy-url");
+			opts.headers.Authorization.should.equal("dummy-token");
+			opts.headers.VRE_ID.should.equal("dummy-vre");
+			callback.should.equal(serverActions.receiveUserData);
+		});
+
+		api.fetchUserData("dummy-url", "dummy-token", "dummy-vre");
+		api.performXhr.restore();
 	});
 });
